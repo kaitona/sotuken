1e873bd71e1d97f628bebf5761ecf2447eb7772d
kaitona <kaito146remi@gmail.com>
2023-04-22 22:43:59 +0900

    add upload

--- pythonDjangoDevelopEnv/code/mysite/imagedb/urls.py
+++ pythonDjangoDevelopEnv/code/mysite/imagedb/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'imagedb'
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'upload/', views.upload, name='upload'),
 ]
--- pythonDjangoDevelopEnv/code/mysite/imagedb/views.py
+++ pythonDjangoDevelopEnv/code/mysite/imagedb/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'imagedb/index.html')
+
+def upload(request):
+    return render(request, 'imagedb/upload.html')
