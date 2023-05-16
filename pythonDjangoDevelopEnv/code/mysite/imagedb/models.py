badf9bfbe1e7b161a3bc89df95c1d8a5de0583ae
R N <ryosuke.geek@gmail.com>
2023-05-16 15:00:11 +0900

    画像のurlをハッシュ値に

--- pythonDjangoDevelopEnv/code/mysite/imagedb/models.py
+++ pythonDjangoDevelopEnv/code/mysite/imagedb/models.py
@@ -1,13 +1,43 @@
 from django.db import models
 
+from django.conf import settings
+import hashlib 
+from django.core.files.storage import default_storage
+import os
 # Create your models here.
 class UploadImage(models.Model):
     title = models.CharField(max_length=200)
 
     description = models.TextField()
-    image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to="")
     image_hash = models.CharField(max_length=64, default="default")
 
-    def calculate_hash(self):
-        # 再計算ロジック
-        self.image_hash = self.column1 + self.column2
+   
+    
+
+    def save(self, *args, **kwargs):
+        pass
+        print ("create_model")
+
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+
+        image_pass = settings.MEDIA_ROOT + str(self.image)
+        print(image_pass)
+        with open(image_pass, 'rb') as f:
+            sha256 = hashlib.sha256(f.read()).hexdigest()
+            print(sha256)
+            self.image_hash=sha256
+
+
+        # 移動元と移動先のファイルパスを取得する
+        source_path = default_storage.path(str(self.image.name))
+        destination_path = default_storage.path( str(sha256)+".png")
+
+        # os.rename()を使用してファイルを移動する
+        os.rename(source_path, destination_path)
+        self.image=sha256+".png"
+
+
+        
+        super().save()
+            
