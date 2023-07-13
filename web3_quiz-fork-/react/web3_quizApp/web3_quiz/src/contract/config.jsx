e54558efef48404600d2ba4ba5a54b2a17dcd23f
kaitona <kaito146remi@gmail.com>
2023-07-13 15:03:16 +0900

    abi update

--- web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/contract/config.jsx
+++ web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/contract/config.jsx
@@ -1,10 +1,9 @@
-import BloctoSDK from "@blocto/sdk";
-const chainId= "0x13881"; // (required) chainId to be used
-const rpc="https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
+const chainId = "0x13881"; // (required) chainId to be used
+const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
 
 
 
-const quiz_address="0x2CebED06D2c721D9a5553D455665d7c1fb66e52B";
-const token_address="0x3872813914A06BB13D44f5a8b610F64735DdA048";
+const quiz_address = "0x9A108973978Fe5A8ae16f58cbCc92CA2305BdE0E";
+const token_address = "0xA63551e91e360274BE55391dD2A4A93D308Adb01";
 
-export {chainId,rpc,quiz_address,token_address };
+export { chainId, rpc, quiz_address, token_address };
