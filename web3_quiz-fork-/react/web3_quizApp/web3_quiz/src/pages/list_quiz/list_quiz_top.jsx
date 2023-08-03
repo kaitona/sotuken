c3756bd69d45e45fcf88d316364bcb8a301dff12
R N <ryosuke.geek@gmail.com>
2023-08-03 16:50:25 +0900

    fix contracts

--- web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/pages/list_quiz/list_quiz_top.jsx
+++ web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/pages/list_quiz/list_quiz_top.jsx
@@ -1,34 +1,33 @@
-import { Contracts_MetaMask } from "../../contract/contracts"
-import Form from 'react-bootstrap/Form';
-import { useState, useEffect, useRef } from 'react';
-import MDEditor, { selectWord } from "@uiw/react-md-editor";
-import { resolvePath, useParams } from "react-router-dom";
+import {Contracts_MetaMask} from "../../contract/contracts";
+import Form from "react-bootstrap/Form";
+import {useState, useEffect, useRef} from "react";
+import MDEditor, {selectWord} from "@uiw/react-md-editor";
+import {resolvePath, useParams} from "react-router-dom";
 import Simple_quiz from "./components/quiz_simple";
 import Quiz_list from "./components/quiz_list";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function List_quiz_top(props) {
-
     //クイズのコントラクト
-    let cont = new Contracts_MetaMask;
+    let cont = new Contracts_MetaMask();
 
     //現在表示している個数を保持するref
-    const now_numRef = useRef(0);//保存
+    const now_numRef = useRef(0); //保存
     //クイズの総数
-    const [quiz_sum, Set_quiz_sum] = useState(null);//保存
+    const [quiz_sum, Set_quiz_sum] = useState(null); //保存
 
     //表示するクイズのリスト
-    const [quiz_list, Set_quiz_list] = useState([]);//保存
+    const [quiz_list, Set_quiz_list] = useState([]); //保存
     //１回の更新で追加で表示する個数
     const [add_num, Set_add_num] = useState(7);
     // コンテナのrefを作成
     const containerRef = useRef(null);
 
     const callback = (entries, observer) => {
-        entries.forEach(entry => {
+        entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 // ターゲットの<div>が画面に表示された場合に実行される関数
-                console.log('Target div is visible on the screen!');
+                console.log("Target div is visible on the screen!");
                 // ここに実行したい関数を記述
 
                 // console.log("///////",now_num);
@@ -36,25 +35,19 @@ function List_quiz_top(props) {
         });
     };
 
-
     useEffect(() => {
-        cont.get_quiz_lenght().then(data => {
+        cont.get_quiz_lenght().then((data) => {
             // Promise オブジェクトが解決された後の処理を記述
             console.log(Number(data));
             let now = parseInt(Number(data));
             Set_quiz_sum(now);
             now_numRef.current = now;
-
         });
-            
     }, []);
 
     const targetRef = useRef(null); // ターゲット要素のrefを作成
 
-
-
     if (quiz_sum != null) {
-
         return (
             <>
                 {/* スクロールを監視するコンポーネント */}
@@ -63,29 +56,17 @@ function List_quiz_top(props) {
                 {/* */}
                 {quiz_list.map((quiz, index) => {
                     if (index !== quiz_list.length - add_num) {
-                        return (
-                            <>
-                                {quiz_list[index]}
-                            </>
-                        );
+                        return <>{quiz_list[index]}</>;
                     }
-                }
-                )}
+                })}
                 <div ref={targetRef}>
                     {/* ターゲット要素aの内容 */}
                     now_loading
                 </div>
             </>
         );
+    } else {
+        return <></>;
     }
-    else {
-        return (
-            <>
-
-            </>
-        );
-    }
-
-
 }
 export default List_quiz_top;
--- web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/pages/navbar/navbar.jsx
+++ web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/pages/navbar/navbar.jsx
@@ -1,92 +1,89 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from "react";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import "./navbar.css"
-import {AiFillPropertySafety, AiOutlineUser} from 'react-icons/ai';
-import {MdOutlineQuiz} from 'react-icons/md';
-import {AiOutlineUnorderedList} from 'react-icons/ai'
-import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
-import { AiFillHome, AiFillSetting, AiFillStar } from 'react-icons/ai';
-import {Contracts_MetaMask} from "../../contract/contracts" 
-import Modal_change_network from './Modal_change_network';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import "./navbar.css";
+import {AiFillPropertySafety, AiOutlineUser} from "react-icons/ai";
+import {MdOutlineQuiz} from "react-icons/md";
+import {AiOutlineUnorderedList} from "react-icons/ai";
+import {BrowserRouter as Router, Route, NavLink, Switch} from "react-router-dom";
+import {AiFillHome, AiFillSetting, AiFillStar} from "react-icons/ai";
+import {Contracts_MetaMask} from "../../contract/contracts";
+import Modal_change_network from "./Modal_change_network";
 
 function Nav_menu(props) {
-    const [useing_address,Set_useing_address] =useState(null);
-    const [chain_id,setChain_id]=useState(null) ;
-    
+    const [useing_address, Set_useing_address] = useState(null);
+    const [chain_id, setChain_id] = useState(null);
 
     useEffect(() => {
-      //非同期処理をUseEffect内で行う場合は、async/awaitを使用する
-      const get_variable = async () => {
-        
-          setChain_id(await props.cont.get_chain_id());
-          Set_useing_address(await props.cont.get_address());
-      }
-      get_variable();
-    }, [])
+        //非同期処理をUseEffect内で行う場合は、async/awaitを使用する
+        const get_variable = async () => {
+            setChain_id(await props.cont.get_chain_id());
+            Set_useing_address(await props.cont.get_address());
+        };
+        get_variable();
+    }, []);
 
     return (
         <>
-        <Modal_change_network chain_id={chain_id} cont={props.cont}/>
-        <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <Navbar
-          fixed="bottom"
-          bg="light"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-        <div className='row justify-content-center '>
-            <div className='col-3'>
-               
-                <Nav.Item>
-                  <Nav.Link eventKey="/list_quiz" href="/list_quiz">
-                    <div className='col-12 '>
-                      <AiOutlineUnorderedList size={30}/>
+            <Modal_change_network chain_id={chain_id} cont={props.cont} />
+            <div style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+                <Navbar
+                    fixed="bottom"
+                    bg="light"
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <div className="row justify-content-center ">
+                        <div className="col-3">
+                            <Nav.Item>
+                                <Nav.Link eventKey="/list_quiz" href="/list_quiz">
+                                    <div className="col-12 ">
+                                        <AiOutlineUnorderedList size={30} />
+                                    </div>
+                                    <div className="col-12 d-flex justify-content-center align-items-center">
+                                        <font size="2">一覧</font>
+                                    </div>
+                                </Nav.Link>
+                            </Nav.Item>
+                        </div>
+                        <div className="col-1"></div>
+
+                        <div className="col-3">
+                            <Nav.Item>
+                                <Nav.Link eventKey="/create_quiz" href="/create_quiz">
+                                    <div className="col-12 ">
+                                        <MdOutlineQuiz size={30} />
+                                    </div>
+                                    <div className="col-12 d-flex justify-content-center align-items-center">
+                                        <font size="2">出題</font>
+                                    </div>
+                                </Nav.Link>
+                            </Nav.Item>
+                        </div>
+                        <div className="col-1"></div>
+                        <div className="col-3">
+                            <Nav.Item>
+                                <Nav.Link eventKey="user_page" href={"/user_page/" + useing_address}>
+                                    <div className="col-12">
+                                        <AiOutlineUser size={30} />
+                                    </div>
+                                    <div className="col-12 d-flex justify-content-center align-items-center">
+                                        <font size="2">myPage</font>
+                                    </div>
+                                </Nav.Link>
+                            </Nav.Item>
+                        </div>
                     </div>
-                    <div className='col-12 d-flex justify-content-center align-items-center'>
-                      <font size="2">一覧</font>
-                    </div>
-                    </Nav.Link>
-                </Nav.Item>
+                </Navbar>
             </div>
-            <div className='col-1'></div>
-            
-            <div className='col-3'>
-                <Nav.Item>
-                  <Nav.Link eventKey="/create_quiz" href="/create_quiz">
-                    <div className='col-12 '>
-                      <MdOutlineQuiz size={30}/>
-                    </div>
-                    <div className='col-12 d-flex justify-content-center align-items-center'>
-                      <font size="2">出題</font>
-                    </div>
-                  </Nav.Link>
-                </Nav.Item>
-            </div>
-            <div className='col-1'></div>
-            <div className='col-3'>
-                <Nav.Item>
-                  <Nav.Link eventKey="user_page" href={"/user_page/"+useing_address}>
-                    <div className='col-12'>
-                      <AiOutlineUser size={30}/>
-                    </div>
-                    <div className='col-12 d-flex justify-content-center align-items-center'>
-                      <font size="2">myPage</font>
-                    </div>
-                  </Nav.Link>
-                </Nav.Item>
-            </div>
-        </div>
-      </Navbar>
-      </div>
-      </>
+        </>
     );
 }
 
