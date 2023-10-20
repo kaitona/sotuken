107c55e4512fdb11d35c22440c5a40f11c52fd03
kaitona <kaito146remi@gmail.com>
2023-10-20 11:48:29 +0900

    deploy

--- web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/contract/config.jsx
+++ web3_quiz-fork-/react/web3_quizApp/web3_quiz/src/contract/config.jsx
@@ -1,7 +1,7 @@
 const chainId = "0x13881"; // (required) chainId to be used
 const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
 
-const quiz_address = "0xfbA60F3F510D30c0A4dFa6a645aBF0823F264061";
-const token_address = "0x60e4999c31f497c02b784E9B138dC000d30d2068";
+const quiz_address = "0xF4EDfD8E1946B498c17b15E3E84283E3E38e1a7b";
+const token_address = "0x9A108973978Fe5A8ae16f58cbCc92CA2305BdE0E";
 
 export { chainId, rpc, quiz_address, token_address };
